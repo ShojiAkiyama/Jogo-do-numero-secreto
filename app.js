b0d189dc9343685c2f9dc1427810af1055b77fa1
ShojiAkiyama <shoji.akiyama@hotmail.com>
2024-10-01 15:49:41 -0300

    alterando limite para 100

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do número secreto';
 
 let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+paragrafo.innerHTML = 'Escolha um número entre 1 e 100';
 
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10
+let numeroLimite = 100
 let numeroSecreto = gerarNumeroAleatorio()
 let tentativa = 1;
 
@@ -19,7 +19,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 function verificarChute() {
